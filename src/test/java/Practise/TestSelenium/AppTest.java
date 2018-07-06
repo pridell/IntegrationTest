5ef6ecfea9204507858ae730ab1e848d2e43ecae
Priyam <Priyam@192.168.1.4>
2018-07-06 10:41:57 +0530

    Commit done

---
+++ src/test/java/Practise/TestSelenium/AppTest.java
@@ -0,0 +1,41 @@
+package Practise.TestSelenium;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+    	System.out.println("Test Complete -1");
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+    	System.out.println("Test Complete");
+        assertTrue( true );
+        
+    }
+}
---
+++ src/test/java/automation/Sampletest.java
@@ -0,0 +1,22 @@
+package automation;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Sampletest {
+  @Test
+  public void f() {
+	  
+	 System.out.println("TestNG Sample test");
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
